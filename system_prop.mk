b62365cbc8b4e418908a7b2610ab63a2698305b4
Nick C <ctwoon@pixelexperience.org>
2020-06-18 17:43:21 +0530

    Z01R: props: enable zygote preforking
    faster app launching, as referenced in https://forum.xda-developers.com/asus-zenfone-max-pro-m1/how-to/android-10-faster-app-launch-enabling-t4108817

--- system_prop.mk
+++ system_prop.mk
@@ -102,3 +102,37 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Vendor
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
+
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.gralloc.gfx_ubwc_disable=0 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.enable_gl_backpressure=1 \
+    debug.sf.early_app_phase_offset_ns=1500000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.early_phase_offset_ns=1500000 \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    persist.vendor.color.matrix=2 \
+    debug.cpurend.vsync=false
+
+# ART
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.dex2oat-filter=speed \
+    dalvik.vm.image-dex2oat-filter=speed \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.sys.fw.dex2oat_thread_count=8 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-threads=8
+
+# Boot
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.shutdown.waittime=500
+
+# Zygote preforking
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
+
